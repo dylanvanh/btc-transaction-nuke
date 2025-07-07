37c68f494d4e076c3541891d0cf2e962ede28368
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-07-08 00:08:16 +0200

    refactor: remove unused NEXT_PUBLIC_BASE_URL environment variable

--- src/app/env.ts
+++ src/app/env.ts
@@ -5,11 +5,8 @@ export const env = createEnv({
   server: {
     MEMPOOL_URL: z.string().url(),
   },
-  client: {
-    NEXT_PUBLIC_BASE_URL: z.string().min(1),
-  },
+  client: {},
   runtimeEnv: {
     MEMPOOL_URL: process.env.MEMPOOL_URL,
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 });
